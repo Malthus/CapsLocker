42a8dc62571e0a5e4d51af0fa13ed2a3344af6c6
Malthus <github@marijnhubert.nl>
2021-03-06 14:56:17 +0100

    Removed obsolete references to the win32com library.

--- capslocker.py
+++ capslocker.py
@@ -1,10 +1,14 @@
 # pip install pynput
 # pip install pystray
 
+import ctypes
+
 from pynput import keyboard
-from keylock import KeyLockState
 from pystray import Icon, Menu, MenuItem
 from PIL import Image, ImageDraw
+
+
+CAPSLOCK = 0x14
 
 
 class CapsLocker(object):
@@ -12,10 +16,12 @@ class CapsLocker(object):
     def __init__(self):
         systraymenu = Menu(MenuItem("Toggle", self.togglelock), MenuItem("Quit", self.quit))
 
+        self.user32 = ctypes.WinDLL('user32')
+        self.user32.GetKeyState.restype = ctypes.c_short
+
         self.lock = True;
         self.lockedicon = Image.open("CapsLockerOn.png")
         self.unlockedicon = Image.open("CapsLockerOff.png")
-        self.keylockstate = KeyLockState()
         
         self.keyboard = keyboard.Controller()
         self.listener = keyboard.Listener(on_release = self.watchcapslock)
@@ -27,12 +33,16 @@ class CapsLocker(object):
 
 
     def watchcapslock(self, key):
-        if self.lock and self.keylockstate.getcapslock() and key == keyboard.Key.caps_lock:
+        if self.lock and self.getcapslock() and key == keyboard.Key.caps_lock:
             self.switchcapslock()
 
     def switchcapslock(self):
         self.keyboard.press(keyboard.Key.caps_lock)
         self.keyboard.release(keyboard.Key.caps_lock)
+
+    def getcapslock(self):
+        keystate = self.user32.GetKeyState(CAPSLOCK)
+        return (keystate & 0xffff) != 0
 
     def togglelock(self):
         if self.lock:
@@ -41,7 +51,7 @@ class CapsLocker(object):
         else:
             self.lock = True
             self.systray.icon = self.lockedicon
-            if self.keylockstate.getcapslock():
+            if self.getcapslock():
                 self.switchcapslock()
 
     def quit(self):
